a18a451f7a6a25d65f47d4b279e407e52f772a8b
Nikko Adrian <nikkoadrian02@gmail.com>
2024-01-12 09:26:41 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -63,7 +63,7 @@
                 <p>Siswa Baru TKJ</p>
               </div>
               <div class="icon">
-                <i class="ion ion-ios-cloud"></i>
+                <i class="ion ion-android-globe"></i>
               </div>
               <a href="data_ppdb" class="small-box-footer">Lihat lebih <i class="fas fa-arrow-circle-right"></i></a>
             </div>
