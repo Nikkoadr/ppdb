f16a5889315095b1ae690230992721b2eca0ccc4
Nikko Adrian <nikkoadrian02@gmail.com>
2024-02-23 14:45:45 +0700

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -34,11 +34,15 @@ return [
             'driver' => 'local',
             'root' => storage_path('app'),
         ],
+        'local' => [
+            'driver' => 'local',
+            'root' => '/var/nfs/sharing/ppdb_file',
+        ],
 
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
 
