bcb85538b9994e1db6b1d66ca5d5153853c6acf0
Nikko Adrian <nikkoadrian02@gmail.com>
2024-11-06 10:29:59 +0700

    update

--- resources/views/layouts/admin/partials/aside.blade.php
+++ resources/views/layouts/admin/partials/aside.blade.php
@@ -11,11 +11,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          @if (Auth::user()->pasfoto)
-            <img src="{{ asset('storage/dokumen-ppdb/'. Auth::user()->pasfoto) }}" class="img-circle elevation-2" alt="User Image">
-          @else
             <img src="{{ asset('assets/img/defaultpp.png') }}" class="img-circle elevation-2" alt="User Image">
-          @endif
         </div>
         <div class="info">
           <a href="#" class="d-block">{{ Auth::user()->nama }}</a>
